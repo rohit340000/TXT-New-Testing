7698e4015f3df7258b0ee9329aa09b54ee83098d
rohit340000 <164490543+rohit340000@users.noreply.github.com>
2024-03-28 09:12:56 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
-api_id = 26435700
-api_hash = "527cf5174e120a9093611bc69d7b7709"
-bot_token = "6763408781:AAEMgOCwslBalYr2dpxzGRMZPpBEDm8NDhA"
-AUTH_USERS = "5033114900"
+api_id = 27100684
+api_hash = "3d9ff72941b7c8ecafbadf7d625f0108"
+bot_token = "6676623064:AAGNN2UmMZL1qoEoXu30dm8F21xse0Fells"
+AUTH_USERS = "2113261793"
